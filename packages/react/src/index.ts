ad4de08d6b877bfa9ac8ada0fc7009efcd9bba05
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2023-12-19 16:45:58 +0530

    chore(react): pulled out the folder

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,5 @@
-export * from "./lib";
+export { FibrForm } from "./FibrForm";
+export { FibrProvider } from "./FibrProvider";
+export { Fields } from "./Fields";
+export { RenderField } from "./RenderField";
+export type { FieldProps } from "./types";
