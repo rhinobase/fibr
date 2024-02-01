7e7fd784043ba8b0e755556360b1e9af9d174322
Aditya Mathur <mathuraditya724@gmail.com>
2024-02-01 11:41:06 +0530

    chore(blocks): improved text story

--- packages/blocks/src/components/text/text.stories.tsx
+++ packages/blocks/src/components/text/text.stories.tsx
@@ -3,10 +3,66 @@ import { Meta, StoryObj } from "@storybook/react";
 import { f, plugin } from "..";
 import type { Text } from "./Text";
 
+const content = `# Heading level 1 
+## Heading level 2
+### Heading level 3
+#### Heading level 4
+##### Heading level 5
+###### Heading level 6
+
+I just love **bold text**.
+
+Italicized text is the *cat's meow*.
+
+This text is ***really important***.
+
+> Dorothy followed her through many of the beautiful rooms in her castle.
+
+1. First item
+  1. Nested item
+2. Second item
+3. Third item
+4. Fourth item
+
+---
+
+- First item 
+- Second item
+- Third item
+- Fourth item
+
+My favorite search engine is [Duck Duck Go](https://duckduckgo.com).
+
+| Syntax      | Description | Test Text     |
+| :---        | :----:      |      ---:     |
+| Header      | Title       | Here's this   |
+| Paragraph   | Text        | And more      |
+
+I need to highlight these ==very important words==.
+
+- [ ] Write the press release
+- [x] Update the website
+- [ ] Contact the media
+
+<p style="color:blue">Make this text blue.</p>
+
+- [Underline](#underline)
+- [Indent](#indent)
+- [Center](#center)
+- [Color](#color)
+
+At the command prompt, type \`nano\`
+
+\`\`\`jsx
+import { Button } from "@rafty/ui"
+<Button />
+\`\`\`
+`;
+
 const meta: Meta<Text> = {
   title: "fibr / Text",
   args: {
-    value: "This is a sample value",
+    value: content,
     hidden: false,
     tooltip: "This is a text field",
   },
