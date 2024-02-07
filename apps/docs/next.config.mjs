d49334942961a03f886cd4c99635b5dc9d581983
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2024-02-07 23:01:57 +0530

    Revert "feat(web): added docusaurus"
    This reverts commit e9b856100f938f736308180287e566ac320c542a.

---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,35 @@
+import nextMDX from "@next/mdx";
+import { composePlugins, withNx } from "@nx/next";
+
+import { recmaPlugins } from "./mdx/recma.mjs";
+import { rehypePlugins } from "./mdx/rehype.mjs";
+import { remarkPlugins } from "./mdx/remark.mjs";
+import withSearch from "./mdx/search.mjs";
+
+const withMDX = nextMDX({
+  options: {
+    remarkPlugins,
+    rehypePlugins,
+    recmaPlugins,
+  },
+});
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  experimental: {
+    optimizePackageImports: ["@rafty/ui"],
+  },
+  reactStrictMode: false,
+};
+
+const plugins = [withSearch, withMDX, withNx];
+
+export default composePlugins(...plugins)(nextConfig);
