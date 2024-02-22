1a33ce7b7cc0730ab9db9afd7e3ce3bd037729cb
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-02-22 12:30:43 +0530

    chore(canvas): removed mobile button

--- packages/builder/canvas/src/page/index.tsx
+++ packages/builder/canvas/src/page/index.tsx
@@ -2,22 +2,16 @@ import { useDroppable } from "@dnd-kit/core";
 import { useCanvas } from "@fibr/providers";
 import { WeaverProvider } from "@fibr/react";
 import { Canvas, CustomControls } from "@fibr/shared";
-import { eventHandler } from "@rafty/shared";
-import { Button, useBoolean } from "@rafty/ui";
 import { FormProvider, useForm } from "react-hook-form";
-import { HiComputerDesktop, HiDevicePhoneMobile } from "react-icons/hi2";
 import { Diagram } from "./Diagram";
 import { NodeWrapper } from "./NodeWrapper";
 
 export function PageCanvas() {
-  const [isMobile, toggle] = useBoolean(false);
   const methods = useForm();
   const select = useCanvas(({ select }) => select);
   const { setNodeRef } = useDroppable({
     id: "canvas",
   });
-
-  const mobileScreen = eventHandler(() => toggle());
 
   return (
     <FormProvider {...methods}>
@@ -27,18 +21,7 @@ export function PageCanvas() {
             <Diagram />
           </WeaverProvider>
         </div>
-        <CustomControls>
-          <Button
-            title="toggle interactivity"
-            aria-label="toggle interactivity"
-            size="icon"
-            variant="ghost"
-            className="rounded p-1"
-            onClick={mobileScreen}
-          >
-            {isMobile ? <HiComputerDesktop /> : <HiDevicePhoneMobile />}
-          </Button>
-        </CustomControls>
+        <CustomControls />
       </Canvas>
     </FormProvider>
   );
