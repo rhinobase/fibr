7e5b5cda3ae8f8b1df1c26614606e817b4da349e
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2024-02-28 11:31:27 +0530

    chore: updated nx

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,6 @@ const terser = require("@rollup/plugin-terser");
 module.exports = (config) => {
   config.preserveModules = true;
 
-  // config.output = {
-  //   ...config.output,
-  //   entryFileNames: "[name].js",
-  //   chunkFileNames: "[name].js",
-  // };
-
   config.plugins.push(
     preserveDirectives.default(), // For preserving "use client" directives
     terser(), // For minification
