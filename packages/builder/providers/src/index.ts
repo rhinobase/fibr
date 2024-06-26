5d856f56798f5def02ba7d5afad47a0484ac0306
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2024-06-26 17:00:02 +0530

    chore: removed providers package

--- packages/builder/providers/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./builder";
-export * from "./canvas";
-export * from "./clipboard";
-export * from "./events";
-export * from "./shortcuts";
-export * from "./utils";
--- packages/builder/providers/src/utils/env.ts
+++
@@ -1,4 +0,0 @@
-export enum Env {
-  DEVELOPMENT = "development",
-  PRODUCTION = "production",
-}
