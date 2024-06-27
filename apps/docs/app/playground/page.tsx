0391baf26c378923aef37ca445701484adf6d9d5
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2024-06-27 10:26:31 +0530

    chore: minor improvements

--- apps/docs/app/playground/page.tsx
+++ apps/docs/app/playground/page.tsx
@@ -1,23 +1,9 @@
-import { Spinner } from "@rafty/ui";
 import dynamic from "next/dynamic";
-import { Logo } from "../../components/Logo";
+import { LoadingScreen } from "./Loading";
 
 const Playground = dynamic(() => import("./Playground"), {
   ssr: false,
-  loading() {
-    return (
-      <div className="flex h-screen w-full items-center justify-center">
-        <div className="flex items-baseline gap-1">
-          <Logo className="h-8 w-auto" />
-          <span className="text-[40px] font-bold italic">Fibr</span>
-        </div>
-        <div className="fixed bottom-10 left-10 flex items-center gap-2">
-          <Spinner size="sm" />
-          <p className="font-medium">Initializing Editor</p>
-        </div>
-      </div>
-    );
-  },
+  loading: () => <LoadingScreen />,
 });
 
 export default function PlaygroundPage() {
