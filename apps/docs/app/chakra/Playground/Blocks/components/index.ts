0d6300d7d56788f9a3351e96b7cc6eca2a40edb3
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-06-27 17:19:26 +0530

    chore(web): removed blocks from chakra example

--- apps/docs/app/chakra/Playground/Blocks/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { StartNode } from "./Start";
-export { EndNode } from "./End";
-export { TransitNode } from "./Transit";
-export { ConditionNode } from "./Condition";
-export { CustomEdge } from "./CustomEdge";
--- apps/docs/app/chakra/Playground/Blocks/settings/NodeSettings.tsx
+++
@@ -1,3 +0,0 @@
-export function NodeSettings() {
-  return <div>NodeSettings</div>;
-}
