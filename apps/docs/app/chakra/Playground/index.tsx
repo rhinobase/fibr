865e1d56c737b52dc306dcaf83e01c086b68a6cd
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-06-27 12:46:14 +0530

    chore(web): changed playgroud tailwind to chakra and added chakra logo

--- apps/docs/app/chakra/Playground/index.tsx
+++ apps/docs/app/chakra/Playground/index.tsx
@@ -1,11 +1,13 @@
 "use client";
+import { Flex, Link, Spacer, Text } from "@chakra-ui/react";
 import { workflowBlocks, workflowConfig } from "@fibr/blocks";
 import { Workspace } from "@fibr/builder";
 import { WorkflowBuilder } from "@fibr/workflow";
-import { Text } from "@rafty/ui";
-import Link from "next/link";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import NextLink from "next/link";
 import { FaGithub, FaXTwitter } from "react-icons/fa6";
 import { Logo } from "../../../components/Logo";
+import ChakraLogo from "../ChakraLogo";
 
 export default function Playground() {
   return (
@@ -21,16 +23,35 @@ export default function Playground() {
 
 function Header() {
   return (
-    <header className="border-secondary-200 dark:border-secondary-800 flex w-full items-center gap-3 border-b px-2 py-0.5">
-      <a
-        title="Fibr Logo"
-        href="https://www.rhinobase.io/"
-        className="flex select-none items-baseline gap-0.5"
+    <Link
+      title="Fibr Logo"
+      _hover={{ textDecoration: "none" }}
+      href="https://www.rhinobase.io/"
+    >
+      <Flex
+        gap={2}
+        align="center"
+        px={2}
+        py={0.5}
+        w="100%"
+        borderBottomWidth="1px"
+        borderColor="gray.200"
       >
-        <Logo className="h-4 w-auto" />
-        <Text className="text-xl font-bold italic">Fibr</Text>
-      </a>
-    </header>
+        <Flex align="baseline" gap={0.5} userSelect="none">
+          <Logo style={{ height: 16, width: "auto" }} />
+          <Text fontSize="xl" fontWeight={700} fontStyle="italic">
+            Fibr
+          </Text>
+        </Flex>
+        <XMarkIcon height={14} width={14} strokeWidth={3} opacity={0.7} />
+        <Flex align="center" gap={0.5} userSelect="none">
+          <ChakraLogo style={{ height: 20, width: "auto" }} />
+          <Text fontSize="xl" fontWeight={700}>
+            chakra
+          </Text>
+        </Flex>
+      </Flex>
+    </Link>
   );
 }
 
@@ -47,27 +68,38 @@ const SOCIALS = {
 
 function Footer() {
   return (
-    <footer className="border-secondary-200 dark:border-secondary-800 flex w-full select-none items-center border-t text-[0.75rem] leading-[1.25rem]">
-      <Text isMuted>version {process.env.NEXT_PUBLIC_VERSION}</Text>
-      <div className="flex-1" />
-      <Text isMuted>
+    <Flex
+      align="center"
+      w="100%"
+      userSelect="none"
+      borderTopWidth="1px"
+      fontSize="0.75rem"
+      lineHeight="1.25rem"
+      borderColor="gray.200"
+    >
+      <Text opacity={0.6}>version {process.env.NEXT_PUBLIC_VERSION}</Text>
+      <Spacer />
+      <Text opacity={0.6}>
         © {new Date().getFullYear()} rhinobase, Inc. All rights reserved.
       </Text>
-      <div className="flex-1" />
-      <div className="flex items-center gap-2 px-2">
+      <Spacer />
+      <Flex align="center" gap={2} px={2}>
         {Object.entries(SOCIALS).map(([name, { link, icon: Icon }]) => (
-          <Link
-            key={name}
-            href={link}
-            title={name}
-            className="text-secondary-500 dark:text-secondary-400 dark:hover:text-secondary-50 transition-all ease-in-out hover:text-black"
-            target="_blank"
-            rel="noopener"
-          >
-            <Icon size={15} />
-          </Link>
+          <NextLink href={link} passHref key={name}>
+            <Link
+              title={name}
+              color="gray.500"
+              _hover={{ color: "black" }}
+              transition="all"
+              transitionTimingFunction="ease-in-out"
+              target="_blank"
+              rel="noopener"
+            >
+              <Icon size={15} />
+            </Link>
+          </NextLink>
         ))}
-      </div>
-    </footer>
+      </Flex>
+    </Flex>
   );
 }
