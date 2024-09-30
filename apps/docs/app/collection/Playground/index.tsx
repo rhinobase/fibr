b78d9242620da9e4c7b6a25cc78fefbf01c4a5bc
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-09-30 12:32:00 +0530

    Update index.tsx

--- apps/docs/app/collection/Playground/index.tsx
+++ apps/docs/app/collection/Playground/index.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { quackFields } from "@duck-form/fields";
 import { Workspace } from "@fibr/builder";
-import { DuckForm } from "duck-form";
 import { FormProvider, useForm } from "react-hook-form";
 import { Toaster } from "react-hot-toast";
 import {
@@ -49,26 +47,17 @@ export default function CollectionPage() {
   return (
     <>
       <FormProvider {...methods}>
-        <DuckForm
-          components={{
-            ...quackFields,
-            canvas: Canvas,
-            array: ArrayField,
-          }}
-          generateId={(_, props) => (props.id ? String(props.id) : undefined)}
-        >
-          <Workspace className="flex h-screen w-full flex-col">
-            <Builder
-              schema={DEFAULT_SCHEMA}
-              blocks={formBlocks}
-              config={{
-                ...formConfig,
-                canvas: { builder: Canvas, settings: CanvasSettings },
-                array: { builder: ArrayField, settings: CanvasSettings },
-              }}
-            />
-          </Workspace>
-        </DuckForm>
+        <Workspace className="flex h-screen w-full flex-col">
+          <Builder
+            schema={DEFAULT_SCHEMA}
+            blocks={formBlocks}
+            config={{
+              ...formConfig,
+              canvas: { builder: Canvas, settings: CanvasSettings },
+              array: { builder: ArrayField, settings: CanvasSettings },
+            }}
+          />
+        </Workspace>
       </FormProvider>
       <Toaster />
     </>
