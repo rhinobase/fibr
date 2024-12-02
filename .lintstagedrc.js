f32e4c9b4791e6654ffcc3f86cbd698dbaa2d504
LuckySharma014 <117056913+LuckySharma014@users.noreply.github.com>
2024-12-02 12:44:34 +0530

    chore: shifted to nano-staged

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "{apps,libs,tools}/**/*.{js,jsx,ts,tsx,json}": [
-    (files) => `nx affected --target=typecheck --files=${files.join(",")}`,
-    (files) => `pnpm dlx @biomejs/biome check --apply ${files.join(" ")}`,
-  ],
-};
